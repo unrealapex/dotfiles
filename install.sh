7f5baf04942241567bc3e96a929f878448ec3ca6
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-02-18 00:41:14 -0800

    use `~/.bash_profile` instead of `~/.profile`

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Ho
 
 # add homebrew to path
 echo "Adding Homebrew to path..."
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> "${HOME}"/.profile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> "${HOME}"/.bash_profile
 # FIXME: figure out why this is not evaluating properly
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
