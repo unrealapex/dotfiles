f0daf6892d0364d5b0b29918ef09e20e26ee4b5b
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-02-28 19:55:04 +0000

    Disable treesitter if reading a file over 100 kilobytes

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -238,7 +238,13 @@ return {
         auto_install = true,
         highlight = {
           enable = true, -- false will disable the whole extension
-          disable = { "" }, -- list of language that will be disabled
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end
         },
         indent = { enable = true, disable = { "yaml" } },
       }
