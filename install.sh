7a0849d35aa967fbc733b4a545c9b0ec7a8b0c9f
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-03-22 17:55:38 +0000

    Remove Google Chrome deb file after installation

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ fi
 # google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
 && sudo apt -y --fix-missing install ./google-chrome*.deb \
-&& sudo apt-get -y install -f
+&& sudo apt-get -y install -f && rm google-chrome*.deb
 
 # anaconda
 echo "Installing Anaconda..."
