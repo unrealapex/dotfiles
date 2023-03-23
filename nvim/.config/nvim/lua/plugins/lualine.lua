a986a7128be2f90548cfc7375fa33dcf04b2f120
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-03-23 14:01:44 -0700

    Add TODO

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -22,6 +22,7 @@ require('lualine').setup {
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = {
+    -- TODO: use a highlight color here instead of hardcoding it
       {
         require("noice").api.statusline.mode.get,
         cond = require("noice").api.statusline.mode.has,
