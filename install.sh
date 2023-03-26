83ae6cdd4abef8d205667a5fe9b1247eb96195c1
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-03-26 00:40:38 +0000

    Install Spotify wihtout prompting

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ if [[ ! $(grep -s Microsoft /proc/version) ]]; then
   # spotify
   curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-  sudo apt install spotify-client
+  sudo apt install -y spotify-client
   
   # zoom
   echo "Installing Zoom..."
