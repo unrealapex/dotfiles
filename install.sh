146682c197bcddfafd00cbee6d2fd42366cb01b8
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-04-03 18:38:36 -0700

    Remove Google Chrome

--- install.sh
+++ install.sh
@@ -61,12 +61,7 @@ if [[ ! $(grep -s Microsoft /proc/version) ]]; then
     else
       echo "Nerd Font already installed, skipping..."
   fi
-
-  # google chrome
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-  && sudo apt -y --fix-missing install ./google-chrome*.deb \
-  && sudo apt-get -y install -f && rm google-chrome*.deb
-
+  
   # spotify
   curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
