e2a59e6c301d161be0f2dcf695c50137b3257ae6
Aaron <11531485-UnrealApex@users.noreply.gitlab.com>
2023-04-07 18:14:25 +0000

    Remove finished TODO

--- install.sh
+++ install.sh
@@ -38,8 +38,6 @@ backup ~/.config/nvim
 echo "Creating symlinks..."
 stow */
 
-# TODO: remove files already present
-
 # homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
