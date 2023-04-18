5c48e6bd066eeed6585881ef875f9c826b2b4f4e
Aaron <11531485-UnrealApex@users.noreply.gitlab.com>
2023-04-18 22:30:04 +0000

    Add note

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ cd
 sudo apt install -y git && git clone https://www.gitlab.com/UnrealApex/dotfiles.git "$HOME"/.dotfiles
 cd "$HOME"/.dotfiles || echo "Unable to enter dotfiles directory, please check what is wrong"; exit
 
+# NOTE: perhaps use the testing version instead of stable? 
 echo "deb http://deb.debian.org/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list
 # debian backports
 echo "deb http://deb.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list
