033bcfae55b3d2a6978746ba579cc0a6a21d4722
Aaron <11531485-unrealapex@users.noreply.gitlab.com>
2023-04-21 15:43:22 +0000

    Use ~ for home

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 cd
 
-sudo apt install -y git && git clone https://www.gitlab.com/UnrealApex/dotfiles.git "$HOME"/.dotfiles
-cd "$HOME"/.dotfiles || echo "Unable to enter dotfiles directory, please check what is wrong"; exit 1
+sudo apt install -y git && git clone https://www.gitlab.com/UnrealApex/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles || echo "Unable to enter dotfiles directory, please check what is wrong"; exit 1
 
 # NOTE: perhaps use the testing version instead of stable? 
 sudo echo "deb http://deb.debian.org/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list
