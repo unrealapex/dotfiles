715b5349d159e45fa1c3518a89085474119b1c7a
Aaron John <unrealapex@proton.me>
2023-04-27 02:33:21 -0700

    Don't set screenshot of desktop as background if blurring fails

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -2,7 +2,5 @@
 desktop=/tmp/lockscreen.png
 # take a screenshot of the desktop
 import -window root $desktop
-# blur image
-convert $desktop -blur 0x8 $desktop
-# lock computer and set blurred image as lockscreen background
-i3lock -i $desktop
+# blur image and lock computer with blurred image as lockscreen background
+convert $desktop -blur 0x8 $desktop && i3lock -i $desktop || i3lock
