bc97712dcde7b182fb3d89e66ff3ca8d587fc9dc
Aaron <unrealapex@proton.me>
2023-04-27 02:25:35 -0700

    Blur lockscreen background instead of pixelating it

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -2,7 +2,7 @@
 desktop=/tmp/lockscreen.png
 # take a screenshot of the desktop
 import -window root $desktop
-# pixelate image
-convert -scale 10% -scale 1000% $desktop $desktop
-# lock computer and set pixelated image as lockscreen
+# blur image
+convert $desktop -blur 0x8 $desktop
+# lock computer and set blurred image as lockscreen background
 i3lock -i $desktop
