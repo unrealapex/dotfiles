4c4335d051a98097f5aa01b641760341a601d5eb
Aaron John <unrealapex@proton.me>
2023-05-02 21:22:06 -0700

    Update font cache with sudo

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ if [ ! -f "/usr/share/fonts/truetype/JetBrains Mono Nerd Font Complete Regular.t
   cd /usr/share/fonts/truetype
   sudo curl --fail --location --output "JetBrains Mono Nerd Font Complete Regular.ttf" \
   https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Nerd%20Font%20Complete%20Regular.ttf
-  fc-cache -fv
+  sudo fc-cache -fv
   echo "Nerd Font installed"
   cd
   else
