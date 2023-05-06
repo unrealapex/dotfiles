852c64ce1dd1310f2004780a3b1809c664a72329
Aaron John <unrealapex@proton.me>
2023-05-05 22:41:35 -0700

    Move user.js to correct directory

--- install.sh
+++ install.sh
@@ -83,11 +83,15 @@ chmod +x anaconda-installer.sh && \
 rm anaconda-installer.sh && \
 conda config --set auto_activate_base false
 
-# FIXME: move user.js to profile directory
 # install betterfox fastfox and smoothfox
 curl -o user.js https://raw.githubusercontent.com/yokoffing/Betterfox/master/Fastfox.js && \
 curl https://raw.githubusercontent.com/yokoffing/Betterfox/master/Smoothfox.js >> user.js  && \
-mv user.js ~/.mozilla/firefox/user.js
+# ensure profiles directory is created
+sleep 10 && killall firefox &
+firefox -headless &
+wait 
+# move user.js to default firefox profile
+mv user.js "$(find ~/.mozilla/firefox/ -type d -name "*.default-release")"
 
 # set up Git commit information
 echo "Setting Git commit information..."
