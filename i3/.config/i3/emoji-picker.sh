25b7a1bc6f09f440ff7c32b7447fa0fde7743e16
Aaron John <unrealapex@proton.me>
2023-05-10 23:46:27 -0700

    Revert "Use dmenu instead of Rofi"
    This reverts commit 8ee3231670c60bf0e14c2b133cf8f98d5c064eca.

--- i3/.config/i3/emoji-picker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# luke smith is the best
-
-# Get user selection via dmenu from emoji file.
-chosen=$(cut -d ';' -f1 ~/.config/i3/emojis | dmenu -i -l 10 | sed "s/ .*//")
-xdotool type "$chosen"
-
-
