9c03aae8c785ecb6a621fafcb2323dca6f1e7a70
Aaron <11531485-unrealapex@users.noreply.gitlab.com>
2023-05-29 01:14:38 +0000

    Use Google Chrome Instead of Firefox

--- install.sh
+++ install.sh
@@ -127,17 +127,6 @@ chmod +x anaconda-installer.sh && \
 rm anaconda-installer.sh && \
 conda config --set auto_activate_base false
 
-# install betterfox's fastfox and smoothfox user.js and merge it into one file
-curl -o user.js https://raw.githubusercontent.com/yokoffing/Betterfox/master/Fastfox.js && \
-curl https://raw.githubusercontent.com/yokoffing/Betterfox/master/Smoothfox.js >> user.js  && \
-# ensure profiles directory is created
-sleep 10 && killall firefox &
-firefox -headless &
-wait 
-# move user.js to default firefox profile
-mv user.js "$(find ~/.mozilla/firefox/ -type d -name "*.default-release")"
-
-
 # cache lockscreen image
 betterlockscreen --update ~/.dotfiles/sakura.png
 
