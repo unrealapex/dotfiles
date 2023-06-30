2821a5b54a0e58d8c8c0e7fe05e206f951d7f8e9
Aaron <11531485-unrealapex@users.noreply.gitlab.com>
2023-06-30 04:40:36 +0000

    Grep in hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -69,6 +69,11 @@ require("telescope").setup {
     -- builtin picker
     find_files = {
       hidden = true
+    },
+    live_grep = {
+      additional_args = function(opts)
+        return {"--hidden"}
+      end
     }
   },
   extensions = {
