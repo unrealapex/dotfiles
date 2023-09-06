4de1681247d869bdf5e4e66c31a22740647b9d46
Aaron <contactaaron9@gmail.com>
2023-09-06 10:29:06 -0700

    Set up Linux power saving

--- install.sh
+++ install.sh
@@ -205,6 +205,9 @@ fi
 systemctl enable mpd.service
 systemctl --user disable pulseaudio.socket pulseaudio.service
 systemctl --user enable pipewire.socket pipewire-pulse.socket wireplumber.service
+systemctl enable tlp.service
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket
 systemctl enable bluetooth.service
 systemctl enable --now irqbalance.service
 systemctl enable betterlockscreen@$USER
