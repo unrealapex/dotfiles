512f29116696f2cc5ee0ef95e3b31066d9ca0379
Aaron <contactaaron9@gmail.com>
2023-09-08 12:23:47 -0700

    Speed install of AUR packages by increasing cpu cores allocated to makepkg

--- install.sh
+++ install.sh
@@ -32,6 +32,11 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/g' /etc/pacman.conf
 # enable colorful pacman output
 sudo sed -i '/# Color/s/^#//g' /etc/pacman.conf
+
+# allocate more cpu cores to make 
+avail_cores=$(($(nproc) + 1))
+sudo sed -i "s/# MAKEFLAGS=\"-j4\"/MAKEFLAGS=\"-j"$avail_cores"\"/g" /etc/makepkg.conf
+
 sudo pacman -Syu --noconfirm
 
 # install packages
