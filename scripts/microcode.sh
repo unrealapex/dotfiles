6b6fcbf38afd4a9d6bea2fc994ad53cae7bf2133
Aaron <contactaaron9@gmail.com>
2023-09-15 12:56:54 -0700

    Modularize install script
    Split install script into multiple isolated shell scripts that dotbot
    runs

---
+++ scripts/microcode.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Detect CPU vendor and model
+vendor=$(grep vendor_id /proc/cpuinfo | awk '{print $3}' | head -n 1)
+model=$(grep "model name" /proc/cpuinfo | awk -F": " '{print $2}' | head -n 1)
+
+# install microcode
+# Check if the CPU is Intel or AMD
+if [[ $vendor == "GenuineIntel" ]]; then
+   echo "Detected CPU: Intel $model"
+
+   # Install the Intel microcode package
+   sudo pacman -Syu --noconfirm --needed intel-ucode
+
+   # Activate the microcode update
+   sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+elif [[ $vendor == "AuthenticAMD" ]]; then
+   echo "Detected CPU: AMD $model"
+
+   # Install the AMD microcode package
+   sudo pacman -Syu --noconfirm --needed amd-ucode
+
+   # Activate the microcode update
+   sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+else
+   echo "Unsupported CPU: $vendor $model"
+fi
+
+echo "Microcode package installed and activated successfully!"
+
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
---
+++ scripts/services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+systemctl enable gdm.service
+systemctl enable mpd.service
+systemctl --user disable pulseaudio.socket pulseaudio.service
+systemctl --user enable pipewire.socket pipewire-pulse.socket wireplumber.service
+systemctl enable tlp.service
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket
+systemctl enable bluetooth.service
+systemctl enable --now irqbalance.service
+systemctl enable cups.socket
+systemctl enable libvirtd
+
