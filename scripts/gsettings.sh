b71abda404125bd3cd0b4efd2b90a1219a8898a5
Aaron <contactaaron9@gmail.com>
2023-09-16 17:47:48 -0700

    Set clock to 12 hour time

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -3,3 +3,4 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize
 gsettings set org.gnome.shell disable-user-extensions false
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Mauve-dark'
+gsettings set org.gnome.desktop.interface clock-format '12h'
