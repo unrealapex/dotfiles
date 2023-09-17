9deee5e2a397b53cbc0fad3f4db8fc5985d9818f
Aaron <contactaaron9@gmail.com>
2023-09-16 17:46:44 -0700

    Set dark theme for GNOME

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 gsettings set org.gnome.shell disable-user-extensions false
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Mauve-dark'
