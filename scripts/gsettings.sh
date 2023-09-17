c68fefe3808d2ee771b01ccd6e484d9d9ee7a382
Aaron <contactaaron9@gmail.com>
2023-09-16 17:48:57 -0700

    Set cursor

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -4,3 +4,5 @@ gsettings set org.gnome.shell disable-user-extensions false
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Mauve-dark'
 gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.interface cursor-size 24
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
