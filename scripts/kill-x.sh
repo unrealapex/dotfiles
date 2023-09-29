d7be98ad85d91b8173c0dc4b2df737956c12efe0
Aaron <contactaaron9@gmail.com>
2023-09-28 18:06:17 -0700

    Enable killing of Xorg with traditional shortcut

---
+++ scripts/kill-x.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "
+Section "ServerFlags"
+    Option "DontZap" "false"
+EndSection
+
+Section "InputClass"
+    Identifier      "Keyboard Defaults"
+    MatchIsKeyboard "yes"
+    Option          "XkbOptions" "terminate:ctrl_alt_bksp"
+EndSection" | sudo tee /etc/X11/xorg.conf.d/40-kill-x.conf
+
