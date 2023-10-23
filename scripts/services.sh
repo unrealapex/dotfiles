cc7e67124cbb0fd733d09d441264bb546bd365ac
Aaron <contactaaron9@gmail.com>
2023-10-23 00:02:01 -0700

    feat(systemd): run mpd using socket

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 systemctl enable gdm.service
-systemctl --user enable mpd.service
+systemctl --user enable mpd.socket
 systemctl --user disable pulseaudio.socket pulseaudio.service
 systemctl --user enable pipewire.socket pipewire-pulse.socket wireplumber.service
 systemctl enable tlp.service
