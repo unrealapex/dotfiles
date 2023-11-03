0d87dff8e53df1bbda431981a526725c8be70f3e
Aaron John <unrealapex@proton.me>
2023-11-03 10:47:06 -0700

    chore(systemd): remove gdm from services.sh
    remove gdm from services.sh as i do not use gnome

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-systemctl enable gdm.service
 systemctl --user enable mpd.socket
 systemctl --user disable pulseaudio.socket pulseaudio.service
 systemctl --user enable pipewire.socket pipewire-pulse.socket wireplumber.service
