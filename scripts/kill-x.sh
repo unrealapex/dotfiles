cb9ab79edaf0b5bde318034af5d55fac7944e170
Aaron <unrealapex@proton.me>
2023-11-04 03:09:44 +0000

    fix(xorg): ensure quotes in file are not expanded

--- scripts/kill-x.sh
+++ scripts/kill-x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "
+echo '
 Section "ServerFlags"
     Option "DontZap" "false"
 EndSection
@@ -8,5 +8,5 @@ Section "InputClass"
     Identifier      "Keyboard Defaults"
     MatchIsKeyboard "yes"
     Option          "XkbOptions" "terminate:ctrl_alt_bksp"
-EndSection" | sudo tee /etc/X11/xorg.conf.d/40-kill-x.conf
+EndSection' | sudo tee /etc/X11/xorg.conf.d/40-kill-x.conf
 
