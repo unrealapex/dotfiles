722e3dfaae2c3fc014f4746d7195e1c8ed40c970
Aaron <unrealapex@proton.me>
2023-11-03 23:19:17 -0700

    feat(polybar): launch script

---
+++ config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# terminate instances of polybar that are already running
+killall -q polybar
+
+# launch bar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
