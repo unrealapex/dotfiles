9d0fe1af5953f378cd6cfef2b7045089643db92c
Aaron John <unrealapex@proton.me>
2023-11-05 14:51:00 -0800

    feat(scripts): create tf2 setup script

---
+++ scripts/tf2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s /usr/lib32/libSDL2-2.0.so.0 "$HOME/.steam/root/ubuntu12_32/steam-runtime/pinned_libs_32/"
+rm "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/bin/libSDL2-2.0.so.0"
+
