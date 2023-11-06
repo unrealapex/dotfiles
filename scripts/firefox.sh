532388dd09776c02c4af92280be95f2c8ff8bd13
Aaron John <unrealapex@proton.me>
2023-11-06 13:51:48 -0800

    fix(firefox): generate user.js properly

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
 # install betterfox's fastfox and smoothfox user.js and merge it into one file
-curl -o user.js https://raw.githubusercontent.com/yokoffing/Betterfox/master/Fastfox.js && \
-curl https://raw.githubusercontent.com/yokoffing/Betterfox/master/Smoothfox.js >> user.js  && \
+curl -o user.js https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
+
+# append smooth scroll settings and betterfox overrides to user.js
+echo "
+
+
+// smooth scroll
+user_pref("apz.overscroll.enabled", true); // not DEFAULT on Linux
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
+user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
+
+// overrides
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("extensions.pocket.enabled", true);
+user_pref("privacy.userContext.ui.enabled", false);
+" >> user.js
 # ensure profiles directory is created
 sleep 10 && killall firefox &
 firefox -headless &
