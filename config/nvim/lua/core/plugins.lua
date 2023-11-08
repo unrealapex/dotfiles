82ba50da767073342cbf2f95ee830fdf751ec85a
Aaron <unrealapex@proton.me>
2023-11-08 14:21:20 -0800

    tweak(neovim): set transparent colorscheme background

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -588,6 +588,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("tokyonight").setup({
+        transparent = true,
+      })
       vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
