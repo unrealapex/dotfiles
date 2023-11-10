5d9514c6cd6260711435a8104369b6624dd266a1
Aaron <unrealapex@proton.me>
2023-11-09 17:21:06 -0800

    tweak(firefox): highlight all search results

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -31,6 +31,7 @@ user_pref("privacy.userContext.ui.enabled", false);
 user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", true);
 user_pref("browser.toolbars.bookmarks.visibility", "always")
+user_pref("findbar.highlightAll", true)
 ' >> user.js
 
 # ensure profiles directory is created
