7261eed951120d2436373fcb0ccbd4c3271eee01
Aaron John <unrealapex@proton.me>
2023-11-16 19:56:29 -0800

    tweak(tf2): fix tcmalloc.cc error

--- scripts/tf2.sh
+++ scripts/tf2.sh
@@ -2,4 +2,4 @@
 
 ln -s /usr/lib32/libSDL2-2.0.so.0 "$HOME/.steam/root/ubuntu12_32/steam-runtime/pinned_libs_32/"
 rm "$HOME/.local/share/Steam/steamapps/common/Team Fortress 2/bin/libSDL2-2.0.so.0"
-rm "$HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/bin/libtcmalloc_minimal.so.4"
+ln -sf /usr/lib32/libtcmalloc_minimal.so ~/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/bin/libtcmalloc_minimal.so.4
