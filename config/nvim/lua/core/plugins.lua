d487f60a6618fcb80d13822dd9ac3553d59a63a9
Aaron John <unrealapex@proton.me>
2023-11-29 18:42:29 -0800

    fix(neovim): ensure colorscheme is fully transparent

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -575,10 +575,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
     config = function()
-      require("tokyonight").setup({
-        transparent = true,
-      })
       vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
