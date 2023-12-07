e5078ee8516732217f875ef04602623b14e397c7
Aaron <unrealapex@proton.me>
2023-12-07 10:10:57 -0800

    format(neovim): format keymaps.lua

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -27,15 +27,16 @@ end, bufopts)
 -- handle unpack deprecation
 table.unpack = table.unpack or unpack
 function get_visual()
-  local _, ls, cs = table.unpack(vim.fn.getpos('v'))
-  local _, le, ce = table.unpack(vim.fn.getpos('.'))
-  return vim.api.nvim_buf_get_text(0, ls-1, cs-1, le-1, ce, {})
+  local _, ls, cs = table.unpack(vim.fn.getpos("v"))
+  local _, le, ce = table.unpack(vim.fn.getpos("."))
+  return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
 end
 
 vim.keymap.set("v", "<C-r>", function()
   local pattern = table.concat(get_visual())
   -- escape regex and line endings
-  pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"),'\n', '\\n', 'g')
+  pattern =
+    vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
   -- send substitute command to vim command line
   vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//<Left>")
 end)
