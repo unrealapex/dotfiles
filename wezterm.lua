f0acd47ad9c65d3cc8faeae5d891fe306ffb0374
Aaron John <unrealapex@proton.me>
2024-01-13 21:10:21 -0800

    format(wezterm): remove extra line

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,6 @@ local wezterm = require("wezterm")
 -- This table will hold the configuration.
 local config = wezterm.config_builder()
 
-
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
