cd9d32d01372edb4fc1139fdb85ce7897b75230d
Aaron <unrealapex@proton.me>
2024-02-01 11:22:45 -0800

    tweak(neovim): remove plugin vim-cool

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -238,23 +238,6 @@ return {
       vim.cmd([[RainbowParentheses]])
     end,
   },
-  -- turn off search highlighting automatically
-  {
-    "romainl/vim-cool",
-    -- load vim-cool when doing a search
-    keys = {
-      "/",
-      "?",
-      "n",
-      "N",
-      "*",
-      "#",
-      { "*", mode = "v" },
-      { "#", mode = "v" },
-      "g*",
-      "g#",
-    },
-  },
   -- lsp and completion stuff
   {
     "neovim/nvim-lspconfig",
