067d14c359e1e0738469981c976a66ae3e170d2d
Aaron <unrealapex@proton.me>
2024-02-08 09:50:03 -0800

    feat(dwm): actual fullscreen
    fixes issues with fullscreen games' windows going black if you switch
    tags

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -126,6 +126,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_Left,  focusmon,       {.i = -1 } },
