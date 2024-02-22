caba6844cf205dbc4e99b66da70ab5d9fade5e82
Aaron John <unrealapex@proton.me>
2024-02-21 22:26:21 -0800

    feat(neovim): purge lspsaga and use plain lspconfig

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -48,10 +48,15 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- lspconfig mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
--- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
--- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
--- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
--- vim.keymap.set("n", "<space>;", vim.diagnostic.setloclist, opts)
+-- redundtant with inline diagnostics but keeping anyways
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", function()
+  vim.diagnostic.goto_prev({ float = false })
+end, opts)
+vim.keymap.set("n", "]d", function()
+  vim.diagnostic.goto_next({ float = false })
+end, opts)
+vim.keymap.set("n", "<space>;", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -62,19 +67,23 @@ local lsp_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-  -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-  -- vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  -- vim.keymap.set("n", "<space>gi", vim.lsp.buf.implementation, bufopts)
-  -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-  -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-  -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-  -- vim.keymap.set("n", "<space>wl", function()
-  -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, bufopts)
-  -- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-  -- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-  -- vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gI", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set({ "n", "v" }, "<space>ca", function()
+    vim.lsp.buf.code_action({
+      context = { only = { "quickfix", "refactor", "source" } },
+    })
+  end, bufopts)
   vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
@@ -100,14 +109,12 @@ require("mason-lspconfig").setup_handlers({
   }),
 })
 
--- disable neovim lsp"s inline diagnostics and use lspsaga"s diagnostics ui instead
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-  })
-
-local signs = { Error = "󰅚", Warn = "󰀪", Hint = "", Info = " " }
+-- diagnostic text highlight is given to the line number
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- NOTE: maybe disable gutter signs entirely
+-- vim.diagnostic.config({signs = false})
