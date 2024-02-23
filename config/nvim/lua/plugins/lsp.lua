c6cebec9b2bcb8aafae67639218140182c637dc5
Aaron John <unrealapex@proton.me>
2024-02-23 15:35:37 -0800

    refactor(neovim): diagnostic line number coloring

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -110,10 +110,13 @@ require("mason-lspconfig").setup_handlers({
 })
 
 -- diagnostic text highlight is given to the line number
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+for _, diag in ipairs({ "Error", "Warn", "Info", "Hint" }) do
+    vim.fn.sign_define("DiagnosticSign" .. diag, {
+        text = "",
+        texthl = "DiagnosticSign" .. diag,
+        linehl = "",
+        numhl = "DiagnosticSign" .. diag,
+    })
 end
 
 vim.diagnostic.config({
