18a91e6d3dc3710b59c74db2ffc08bf5c8778ca6
Aaron John <unrealapex@proton.me>
2024-02-23 13:05:15 -0800

    feat(neovim): java debugging

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -140,3 +140,13 @@ dap.adapters.bashdb = {
   name = 'bashdb';
 }
 
+dap.adapters.java = function(callback)
+  -- FIXME:
+  -- Here a function needs to trigger the `vscode.java.startDebugSession` LSP command
+  -- The response to the command must be the `port` used below
+  callback({
+    type = 'server';
+    host = '127.0.0.1';
+    port = port;
+  })
+end
