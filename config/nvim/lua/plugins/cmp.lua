c4d50b3de98cb8a35b23297de0932cef4302f886
Aaron John <unrealapex@proton.me>
2024-02-23 11:33:38 -0800

    tweak(neovim): nvim-cmp prioritize signature help

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -68,8 +68,8 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
-    { name = "nvim_lua" },
     { name = "nvim_lsp_signature_help" },
+    { name = "nvim_lua" },
     { name = "buffer" },
     { name = "path" },
     { name = "calc" },
