9a3f8b3894c55b2f0cb7af2301ada31542282ccc
Aaron <unrealapex@proton.me>
2024-02-23 13:26:55 -0800

    feat(neovim): firefox debugging

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -8,6 +8,7 @@ require("mason-nvim-dap").setup({
   ensure_installed = {
     "bash-debug-adapter",
     "java-debug-adapter",
+    "firefox-debug-adapter",
     "debugpy",
     "codelldb",
   },
@@ -189,3 +190,23 @@ dap.adapters.java = function(callback)
     port = port,
   })
 end
+
+dap.adapters.firefox = {
+  type = "executable",
+  command = "node",
+  args = {
+    os.getenv("HOME") .. "/mason/packages/vscode-firefox-debug/dist/adapter.bundle.js",
+  },
+}
+
+dap.configurations.typescript = {
+  {
+    name = "Debug with Firefox",
+    type = "firefox",
+    request = "launch",
+    reAttach = true,
+    url = "http://localhost:3000",
+    webRoot = "${workspaceFolder}",
+    firefoxExecutable = "/usr/bin/firefox",
+  },
+}
