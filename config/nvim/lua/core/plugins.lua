44e6883a674459b75b15028ec3db431210bd76f8
Aaron John <unrealapex@proton.me>
2024-02-28 15:14:13 -0800

    chore(neovim): more conform setup to conform.lua

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -339,31 +339,9 @@ return {
         end,
       },
       mode = { "n", "v" },
-    },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettierd", "prettier" },
-        typescript = { "prettierd", "prettier" },
-        html = { "prettierd", "prettier" },
-        css = { "prettierd", "prettier" },
-        markdown = { "prettierd", "prettier" },
-        python = {
-          formatters = { "isort", "black" },
-          run_all_formatters = true,
-        },
-        sh = { "shfmt" },
-        json = { "jq" },
-        java = { "google-java-format" },
-        c = { "clang-format" },
-        cs = { "clang-format" },
-        cpp = { "clang-format" },
-      },
-      formatters = {
-        shfmt = {
-          prepend_args = { "--indent", "2" },
-        },
-      },
+      config = function ()
+        require("plugins.conform")
+      end
     },
   },
   -- linting
