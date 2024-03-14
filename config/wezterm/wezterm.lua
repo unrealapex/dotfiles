2e3cea55a04f66b95805c9d642a430d1f19c06bd
Aaron John <unrealapex@proton.me>
2024-03-14 15:24:36 -0700

    Revert "tweak(fontconfig): use windows emoji font"
    This reverts commit dc45e84baa596afabb7d5e3b0b20af1ec89df354.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,12 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Campbell'
+-- config.color_scheme = 'AdventureTime'
 
 config.font = wezterm.font_with_fallback({
   -- <built-in>, BuiltIn
   "Jetbrains Mono Nerd Font Mono",
-  "Segoe UI Emoji",
+  "Noto Color Emoji",
 })
 
 config.keys = {
