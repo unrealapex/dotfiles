e091795e4b167cc156b2127e36e4a60b951f586c
Aaron John <unrealapex@proton.me>
2024-03-25 23:36:12 -0700

    Revert "tweak(neovim): use base16 colorscheme"
    This reverts commit 415df6013a15767ffb7808d9f5c7578763b2689e.

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -777,11 +777,15 @@ return {
   "nvim-tree/nvim-web-devicons",
   -- colorscheme
   {
-    "RRethy/base16-nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("base16-windows-95")
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        transparent = true,
+      })
+      vim.cmd.colorscheme("tokyonight-night")
     end,
   },
   -- markdown preview
