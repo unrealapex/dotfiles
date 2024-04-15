1cd3f8c6daf8bfc11c7242edb7107c7a36088baa
Aaron John <unrealapex@proton.me>
2024-04-14 18:20:59 -0700

    tweak(neovim): gitsigns lazy load event

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -125,7 +125,7 @@ return {
       require("plugins.gitsigns")
     end,
     init = function ()
-      vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
         group = vim.api.nvim_create_augroup("load_gitsigns", { clear = true }),
         callback = function ()
           if os.execute("git rev-parse --show-toplevel 2> /dev/null") == 0 then
