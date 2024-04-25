c96d48adb4b1720e47b03c2de545bd846625af0a
Aaron John <unrealapex@proton.me>
2024-04-25 00:05:51 -0700

    fix(neovim): load zen-mode.nvim with pcall

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -116,10 +116,7 @@ function prose()
     vim.keymap.set({ "n", "v" }, "^", "g^")
 
     prosed = true
-    -- FIXME: zen mode is not loaded when called here
-    if vim.fn.exists("ZenMode") == 2 then
-      vim.cmd.ZenMode()
-    end
+    pcall(vim.cmd.ZenMode())
     vim.notify("prose mode: enabled")
   else
     -- disable spellcheck and line wrapping
@@ -134,9 +131,7 @@ function prose()
     vim.keymap.del({ "n", "v" }, "$")
     vim.keymap.del({ "n", "v" }, "^")
     prosed = false
-    if vim.fn.exists("ZenMode") == 2 then
-      vim.cmd.ZenMode()
-    end
+    pcall(vim.cmd.ZenMode())
     vim.notify("prose mode: disabled")
   end
 end
