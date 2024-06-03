8275672c05054cf5d75e069b89f65c550bc2fc41
Aaron John <unrealapex@proton.me>
2024-06-02 18:24:42 -0700

    Revert "tweak(neovim): use default colorscheme"
    This reverts commit d8d0d3cfa1f8b9264b265d67aad4d7539d5fe19a.

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+---@diagnostic disable: different-requires, mixed_table, undefined-field
+
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("tokyonight").setup({
+      transparent = true,
+      on_highlights = function(highlights, colors)
+        highlights.StatusLine = colors.Normal
+        highlights.StatusLineNC = colors.Normal
+        highlights.WinBar = {
+          bg = colors.none,
+        }
+        highlights.WinBarNC = {
+          bg = colors.none,
+        }
+      end,
+    })
+    vim.cmd.colorscheme("tokyonight-night")
+  end,
+}
