fcda1a052809c7eff902162470d1fb0b9b583e8a
Aaron John <unrealapex@disroot.org>
2024-07-09 20:10:16 -0700

    fix(neovim): gitsigns config

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -235,9 +235,6 @@ return {
           delay = 1000,
           ignore_whitespace = false,
         },
-        current_line_blame_formatter_opts = {
-          relative_time = false,
-        },
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
