9169b36285ede7b949c1f30d06bac5b970ddf697
Aaron John <unrealapex@disroot.org>
2024-08-10 03:52:19 -0700

    chore(neovim): remove redundant comments

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -1,16 +1,10 @@
--- reread file if it has been modified outside of Vim
 vim.opt.autoread = true
--- set window background to dark
 vim.opt.background = "dark"
--- more powerful backspacing
 vim.opt.backspace = "indent,eol,start"
--- store all backup files in one directory
 vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup"
 vim.opt.cindent = true
 vim.opt.cinoptions = "(0"
--- sync with system clipboard
 vim.opt.clipboard = "unnamedplus"
--- enable cursor line
 vim.opt.cursorline = true
 -- store all swap files in one directory
 vim.opt.directory = vim.fn.stdpath("state") .. "/swap"
@@ -24,7 +18,6 @@ vim.opt.foldmethod = "indent"
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.grepprg = "rg --vimgrep"
 vim.opt.guicursor = ""
--- allow hidden buffers
 vim.opt.hidden = true
 vim.opt.history = 500
 -- ignore case unless explicitly stated
@@ -35,47 +28,34 @@ vim.opt.laststatus = 2
 vim.opt.list = true
 vim.opt.listchars = nil
 vim.opt.magic = true
--- show line numbers
 vim.opt.number = true
 -- basic completion
 vim.opt.omnifunc = "syntaxcomplete#Complete"
 vim.opt.path:append(".,**")
 vim.opt.pumheight = 12
--- show relative line numbers
 vim.opt.relativenumber = true
--- show cursor position in status bar
 vim.opt.ruler = true
--- 8 lines above or below cursor when scrolling
 vim.opt.scrolloff = 8
--- indents to next multiple of 'shiftwidth'.
 vim.opt.shiftwidth = 4
--- vim.opt.shiftround = true
 vim.opt.showmode = true
--- show typed command in status bar
 vim.opt.showcmd = true
 vim.opt.signcolumn = "auto:1-2"
 vim.opt.smartcase = true
 vim.opt.softtabstop = 4
--- put new windows right of the current
 vim.opt.splitright = true
 -- TODO: show diagnostics
 vim.opt.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P"
--- set tabs to two spaces
--- vim.opt.tabstop = 2
 -- true color support
 vim.opt.termguicolors = true
--- show file in titlebar
 vim.opt.title = true
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
 -- persistent undo tree
 vim.opt.undofile = true
 vim.opt.undolevels = 500
--- set updatetime to 200 milliseconds
 vim.opt.updatetime = 200
 vim.opt.visualbell = false
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
--- don't wrap lines
 vim.opt.wrap = false
 
 -- syntax highlighting
