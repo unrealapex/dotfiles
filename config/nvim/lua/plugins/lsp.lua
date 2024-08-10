b287be4756005bf4f0a41c4a140707352f8a027e
Aaron John <unrealapex@disroot.org>
2024-08-10 04:15:48 -0700

    perf(neovim): load lsp on filetype

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,7 @@ return {
   -- lsp and completion stuff
   {
     "neovim/nvim-lspconfig",
-    -- FIXME: using event makes lsp not attach to buffers
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    event = "FileType",
     cmd = {
       "LspInfo",
       "LspStart",
