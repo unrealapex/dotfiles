f0b412de6ac1c0c806cb5ba09d899a6965e6cce4
Aaron John <unrealapex@disroot.org>
2024-10-14 16:03:00 -0700

    chore(neovim): remove comments
    remove comments for self documenting lua modules

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,5 @@
 ---@diagnostic disable: different-requires
--- options
 require("core.options")
--- autocommands
 require("core.autocmds")
--- plugins
 require("core.lazy")
--- keymaps
 require("core.keymaps")
