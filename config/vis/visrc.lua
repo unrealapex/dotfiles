71ad1e6900bf99e7a8dab8418b01f087e8d1eb70
Aaron John <unrealapex@disroot.org>
2024-10-16 19:33:36 -0700

    feat(rice): add vis config

---
+++ config/vis/visrc.lua
@@ -0,0 +1,16 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	vis:command('set number')
+	vis:command('set relativenumbers')
+    vis:command('set autoindent')
+	vis:command('set expandtab')
+    vis:command('set tabwidth 4')
+	vis:command('set ignorecase')
+	vis:command('set escdelay 1')
+end)
