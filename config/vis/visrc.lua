c874de713bcdd088c96ca359f8f7b56b286fe737
Aaron John <unrealapex@disroot.org>
2024-10-18 04:46:55 -0700

    feat(vis): load plugins

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -1,6 +1,44 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
 
+-- built in plugins
+require('plugins/filetype')
+require('plugins/complete-filename')
+require('plugins/complete-word')
+
+-- external plugins
+local plug = require('plugins/vis-plug')
+plug.path('~/.local/share/vis/plugins')
+
+local plugins = {
+ { "https://github.com/milhnl/vis-format" },
+ { "https://github.com/lutobler/vis-commentary" },
+ { "https://gitlab.com/muhq/vis-lspc" },
+ { "https://github.com/lutobler/vis-modelines" },
+ { "https://github.com/e-zk/vis-shebang" },
+ { "https://repo.or.cz/vis-surround" },
+ { "https://git.cepl.eu/cgit/vis/vis-jump" },
+ { "https://git.cepl.eu/cgit/vis/vis-yank-highlight" },
+}
+
+
+plug.init(plugins, true)
+
+shebangs = {
+    ["#!/bin/sh"] = "bash"
+}
+
+-- TODO: lazy load this
+ require("plugins/vis-format")
+ require("plugins/vis-commentary")
+ require("plugins/vis-lspc")
+ require("plugins/vis-modelines")
+ require("plugins/vis-shebang")
+ require("plugins/vis-surround")
+ require("plugins/vis-jump")
+ require("plugins/vis-yank-highlight")
+
+
 vis.events.subscribe(vis.events.INIT, function()
 end)
 
