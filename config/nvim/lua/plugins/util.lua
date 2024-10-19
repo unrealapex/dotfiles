c428c3fc90f285a57610180979ee1a9ad8ab8348
Aaron John <unrealapex@disroot.org>
2024-10-18 23:47:12 -0700

    tweak(neovim): remove persistence.nvim

--- config/nvim/lua/plugins/util.lua
+++ config/nvim/lua/plugins/util.lua
@@ -1,35 +1,6 @@
 ---@diagnostic disable: different-requires, mixed_table, undefined-field
 
 return {
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    keys = {
-      {
-        "<leader>qs",
-        function()
-          require("persistence").load()
-        end,
-      },
-      {
-        "<leader>ql",
-        function()
-          require("persistence").load({ last = true })
-        end,
-      },
-      {
-        "<leader>qd",
-        function()
-          require("persistence").stop()
-        end,
-      },
-    },
-    opts = {
-      dir = vim.fn.stdpath("state") .. "/sessions/",
-      options = vim.opt.sessionoptions:get(),
-    },
-  },
-
   -- repeat plugin commands
   {
     "tpope/vim-repeat",
