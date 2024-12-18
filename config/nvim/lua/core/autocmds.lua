a39f2f3712cb90152e791c16700ddcc96b97c89d
Aaron John <unrealapex@disroot.org>
2024-12-17 23:25:00 -0800

    chore(neovim): remove <q> autocmd

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -53,32 +53,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "query", -- :InspectTree
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set(
-      "n",
-      "q",
-      "<cmd>close<cr>",
-      { buffer = event.buf, silent = true }
-    )
-  end,
-})
-
 vim.api.nvim_create_autocmd("CursorMoved", {
   group = vim.api.nvim_create_augroup("clear_search", { clear = true }),
   callback = function()
