62a93f71a25509a1226b75e59b57dc35fea8438d
unrealapex <unrealapex@disroot.org>
2025-05-11 16:48:53 -0700

    feat(neovim): add mini.snippets

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -112,4 +112,25 @@ return {
       },
     },
   },
+
+  -- snippets
+  {
+    "echasnovski/mini.snippets",
+    dependencies = "rafamadriz/friendly-snippets",
+    event = "InsertEnter",
+    setup = function()
+      local gen_loader = require('mini.snippets').gen_loader
+      require('mini.snippets').setup({
+        snippets = {
+          -- Load custom file with global snippets first
+          -- gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+          -- Load snippets based on current language by reading files from
+          -- "snippets/" subdirectories from 'runtimepath' directories.
+          -- NOTE: sometimes snippets don't load for some reason
+          gen_loader.from_lang(),
+        },
+      })
+    end
+  }
 }
