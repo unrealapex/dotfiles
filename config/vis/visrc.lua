8d352387be2dc65178f133c34510f03e1d7a91aa
unrealapex <unrealapex@disroot.org>
2025-07-23 07:16:19 -0700

    format(vis): use tabs instead of spaces

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -4,71 +4,67 @@ require("vis")
 -- bootstrap vis-plug
 local visplugpath = os.getenv("XDG_CONFIG_HOME") .. "/vis/vis-plug"
 local file_exists = function(path)
-        local file = io.open(path)
-        if not file then return false end
-        file:close()
-        return true
+	local file = io.open(path)
+	if not file then
+		return false
+	end
+	file:close()
+	return true
 end
 
-
-if not file_exists(visplugpath)
-then
-  print("vis-plug not found, installing...")
-  os.execute("git clone --filter=blob:none https://github.com/erf/vis-plug " .. visplugpath)
+if not file_exists(visplugpath) then
+	print("vis-plug not found, installing...")
+	os.execute("git clone --filter=blob:none https://github.com/erf/vis-plug " .. visplugpath)
 end
 
 local plug = require("vis-plug")
 
 local plugins = {
-  { "https://github.com/milhnl/vis-format" },
-  { "https://github.com/lutobler/vis-commentary" },
-  { "https://gitlab.com/muhq/vis-lspc", alias = "lspc" },
-  { "https://github.com/lutobler/vis-modelines" },
-  { "https://repo.or.cz/vis-surround" },
-  { "https://git.sr.ht/~mcepl/vis-jump" },
-  -- { "https://git.sr.ht/~mcepl/vis-yank-highlight" },
-  { "https://repo.or.cz/vis-quickfix" },
+	{ "https://github.com/milhnl/vis-format" },
+	{ "https://github.com/lutobler/vis-commentary" },
+	{ "https://gitlab.com/muhq/vis-lspc", alias = "lspc" },
+	{ "https://github.com/lutobler/vis-modelines" },
+	{ "https://repo.or.cz/vis-surround" },
+	{ "https://git.sr.ht/~mcepl/vis-jump" },
+	-- { "https://git.sr.ht/~mcepl/vis-yank-highlight" },
+	{ "https://repo.or.cz/vis-quickfix" },
 }
 
-plug.path(os.getenv('XDG_DATA_HOME') .. "/vis-plug")
+plug.path(os.getenv("XDG_DATA_HOME") .. "/vis-plug")
 plug.init(plugins, true)
-
 
 plug.plugins.lspc.menu_cmd = "vis-menu"
 
 vis:operator_new("=", function(file, range, pos)
-  local status, out, err = vis:pipe(file, range, "fmt")
-  if not status then
-    vis:info(err)
-  else
-    file:delete(range)
-    file:insert(range.start, out)
-  end
-  return range.start -- new cursor location
+	local status, out, err = vis:pipe(file, range, "fmt")
+	if not status then
+		vis:info(err)
+	else
+		file:delete(range)
+		file:insert(range.start, out)
+	end
+	return range.start -- new cursor location
 end, "Formating operator, filter range through fmt(1)")
 
 vis.events.subscribe(vis.events.INIT, function() end)
 
-vis.events.subscribe(
-  vis.events.WIN_OPEN,
-  function(win) -- luacheck: no unused args
-    -- Your per window configuration options e.g.
-    vis:command("set autoindent")
-    vis:command("set cursorline")
-    vis:command("set escdelay 1")
-    vis:command("set ignorecase")
-    vis:command("set number")
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	vis:command("set autoindent")
+	vis:command("set cursorline")
+	vis:command("set escdelay 1")
+	vis:command("set ignorecase")
+	vis:command("set number")
 
-    vis:map(vis.modes.NORMAL, "]d", function()
-        vis:command("lspc-next-diagnostic")
-    end)
-    
-    vis:map(vis.modes.NORMAL, "[d", function()
-        vis:command("lspc-prev-diagnostic")
-    end)
+	vis:map(vis.modes.NORMAL, "]d", function()
+		vis:command("lspc-next-diagnostic")
+	end)
 
-    vis:map(vis.modes.NORMAL, "gh", function()
-        vis:command("lspc-hover")
-    end)
-  end
-)
+	vis:map(vis.modes.NORMAL, "[d", function()
+		vis:command("lspc-prev-diagnostic")
+	end)
+
+	vis:map(vis.modes.NORMAL, "gh", function()
+		vis:command("lspc-hover")
+	end)
+end)
