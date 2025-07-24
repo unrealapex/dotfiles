a7e4a14d41556e739d0ab1cdbbe3be7906516bca
unrealapex <unrealapex@disroot.org>
2025-07-24 03:24:19 -0700

    tweak(neovim): set tabstop = 8

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -32,9 +32,8 @@ vim.opt.showcmd = true
 vim.opt.signcolumn = "auto:1-2"
 vim.opt.smartcase = true
 vim.opt.splitright = true
--- TODO: show diagnostics
 vim.opt.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P"
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.termguicolors = false
 vim.opt.textwidth = 73
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
