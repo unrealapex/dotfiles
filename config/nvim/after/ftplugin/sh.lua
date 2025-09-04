2d7a961fc62edecf64ee7826878ad1573b0b9297
Erin John <unrealapex@disroot.org>
2025-09-03 21:56:10 -0700

    tweak(neovim): insert tabs instead of spaces in shell scripts

--- config/nvim/after/ftplugin/sh.lua
+++ config/nvim/after/ftplugin/sh.lua
@@ -1,7 +1,5 @@
-vim.bo.expandtab = true
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
-vim.bo.tabstop = 2
+vim.bo.expandtab = false
+
 
 if vim.fn.executable("shfmt") then
 	vim.bo.formatprg = "shfmt --indent 2"
