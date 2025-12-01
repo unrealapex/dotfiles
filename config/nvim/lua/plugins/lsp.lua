59160d30d2bff6eb7a0f5a7f12b34d981b0d7938
Erin John <unrealapex@disroot.org>
2025-12-01 00:25:39 -0800

    chore(neovim): remove lsp hover keymap set by default

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -28,7 +28,6 @@ return {
         vim.keymap.set("n", "<space>;", vim.diagnostic.setloclist, bufopts)
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 				vim.keymap.set("n", "gI", vim.lsp.buf.implementation, bufopts)
 				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
