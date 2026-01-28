819daaa106c9a559250822b75ad910c0584d3856
Erin John <unrealapex@disroot.org>
2026-01-28 14:54:39 -0800

    fix(neovim): lua formatprg command

--- config/nvim/after/ftplugin/lua.lua
+++ config/nvim/after/ftplugin/lua.lua
@@ -3,7 +3,6 @@ vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
 
--- FIXME: stylua does not work well with gq formatting
 if vim.fn.executable("stylua") then
-	vim.bo.formatprg = "stylua %"
+	vim.bo.formatprg = "stylua -"
 end
